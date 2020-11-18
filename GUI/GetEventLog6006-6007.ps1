d23228fc27f9565151744b388cf59efc7c0b2843
skyry1 <skyry1@icloud.com>
2020-11-18 16:58:47 +0900

    システムイベントログの検索ロジックを修正 検索日付の0時から23時59分で検索するように変更

--- GUI/GetEventLog6006-6007.ps1
+++ GUI/GetEventLog6006-6007.ps1
@@ -30,17 +30,18 @@ Add-Type -AssemblyName System.Drawing
     $Date.location = "100,30"
     $Date.Format = "Long"
 	$Date.AutoSize = $True
+    $Date.Width = 150
 #endregion
 
 # 出力欄
 #region TextBox
     $txt1 = New-Object TextBox
     $txt1.location = "100,60"
-    $txt1.Width = 200
+    $txt1.Width = 100
 
     $txt2 = New-Object TextBox
     $txt2.location = "100,90"
-    $txt2.Width = 200
+    $txt2.Width = 100
 #endregion
 
 # 確認ボタン
@@ -48,18 +49,32 @@ Add-Type -AssemblyName System.Drawing
 	$btn = New-Object Button
 	$btn.Text = "確認"
 	$btn.Size = "150, 40"
-	$btn.Location = "150, 130"
+	$btn.Location = "100, 120"
 	$btn.AutoSize = $True
 #endregion
 
 # ボタンのクリック
 $button_Click = {
-    #$str = GET-EVENTLog System -After "2020/01/18 12:00:00" -Newest 1| Where-Object{$_.EventId -eq 6005} | select-Object TimeGenerated    
-    $today = Get-Date -Format "MM/dd/yyyy"
+    
+    #初期化
+    $txt1.Text = ""
+    $txt2.Text = ""
+    
+    #$str = GET-EVENTLog System -After "2020/01/18 12:00:00" -Newest 1| Where-Object{$_.EventId -eq 6005} | select-Object TimeGenerated
+    $before = $Date.Value.Date + " 23:59:59"
+    $after = $Date.Value.Date + " 00:00:00"
+    $replace = "@{TimeGenerated="+$Date.Value.Month+"/"+$Date.Value.Day+"/"+$Date.Value.Year+" "
+    Write-Host $before
+    Write-Host $after
+    Write-Host $replace
     try
     {
-        $txt1.Text = GET-EVENTLog System -After $Date.Value.Date| Where-Object{$_.EventId -eq 6005} | select-Object TimeGenerated
-        $txt1.Text = $txt1.Text.Replace("@{TimeGenerated="+$today+" ","").Replace("}","")
+        $result1 = GET-EVENTLog System -After $after -before $before| Where-Object{$_.EventId -eq 6005 -or $_.EventId -eq 7001} | select-Object TimeGenerated
+        $rs1 = $result1.Length
+        write-host $result1
+        Write-Host $rs1
+        $txt1.Text = $result1[$rs1 - 1]
+        $txt1.Text = $txt1.Text.Replace($replace,"").Replace("}","")
     }
     catch
     {
@@ -67,8 +82,10 @@ $button_Click = {
 
     try
     {
-        $txt2.Text = GET-EVENTLog System -After $Date.Value.Date| Where-Object{$_.EventId -eq 6006} | select-Object TimeGenerated
-        $txt2.Text = $txt2.Text.Replace("@{TimeGenerated="+$today+" ","").Replace("}","")
+        $result2 = GET-EVENTLog System -After $after -before $before| Where-Object{$_.EventId -eq 6006 -or $_.EventId -eq 7002} | select-Object TimeGenerated
+        write-host $result2
+        $txt2.Text = $result2[0]
+        $txt2.Text = $txt2.Text.Replace($replace,"").Replace("}","")
     }
     catch
     {
@@ -79,8 +96,8 @@ $btn.Add_Click($button_Click)
 # フォーム
 #region Form
 	$f = New-Object Form
-	$f.Text = "システムイベントログ6006-6007取得"
-	$f.Size = "400, 230"
+	$f.Text = "6005(7001)-6006(7002)"
+	$f.Size = "330, 220"
     $f.MaximumSize  =$f.Size
     $f.MinimumSize  =$f.Size
     $f.MaximizeBox = $false
