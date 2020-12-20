dbf56a6150d2cd35a51e7c7d2e7167d4aeec105d
skyry1 <skyry1@icloud.com>
2020-12-20 22:59:03 +0900

    バグ修正

--- GUI/WindowsBatLister.ps1
+++ GUI/WindowsBatLister.ps1
@@ -5,31 +5,37 @@ Add-Type -AssemblyName System.Drawing
 
 # フォーム情報
 #region designer
-    
+
     # フォントの設定
     $Font = New-Object System.Drawing.Font("メイリオ",8)
 
+    # フォーム
     $form = New-Object Form
-    $form.FormBorderStyle = "FixedToolWindow"
     $form.Text = "バッチ一覧(Made with powershell)"
     $form.MaximizeBox = $false
     $form.MinimizeBox = $false
     $form.StartPosition = "CenterScreen"
-    $form.Size = "550, 125"
+    $form.Size = "565, 150"
+    $form.font = $Font
 
+    # チェックボックス
     $CheckedBox = New-Object CheckBox
     $CheckedBox.Text = "管理者権限で実行"
-    $CheckedBox.size ="250,22" 
-    $CheckedBox.Location = "10, 5"
+    $CheckedBox.size ="150,22" 
+    $CheckedBox.Location = "5, 5"
     $CheckedBox.Checked = $True
     $CheckedBox.font = $Font
     $form.Controls.Add($CheckedBox)
 
     function Form_Load
     {
-        $x = 25
-        $y = 30
-        $i = 1
+        $LocationX = 25
+        $LocationY = 30
+        $i = 1        
+
+        # サブフォルダ含まない
+        #$itemList = Get-ChildItem $dir
+        # サブフォルダ含む
         $itemList = Get-ChildItem $dir -Recurse
         foreach($item in $itemList)
         {
@@ -37,19 +43,21 @@ Add-Type -AssemblyName System.Drawing
             if($item.Extension -eq ".bat")
             {
                 $btn = New-Object Button
-                $btn.Size = "150, 40"
                 $btn.font = $Font
                 $btn.Text = $item.BaseName
                 $btn.Tag = $item.FullName
-                $btn.Location = "$x,$y"
+                $btn.Size = "150, 50"
+                $btn.Location = "$LocationX,$LocationY"
+
+                #3個ボタンを設置したら次の行へ
                 if($i % 3 -eq 0)
                 {
-                    $x = 25
-                    $y += 50
-                    $form.Height += 30
+                    $LocationX = 25
+                    $LocationY += $btn.height + 25
+                    $form.Height += $btn.height + 25
                 } else 
                 {
-                    $x += 175
+                    $LocationX += $btn.Width + 25
                 }
                 $btn.Add_Click({
                      # 管理者権限で実行
@@ -67,8 +75,8 @@ Add-Type -AssemblyName System.Drawing
                 $i += 1
             }
         }
-        $form.MaximumSize  =$form.Size
-        $form.MinimumSize  =$form.Size
+        $form.MaximumSize = $form.Size
+        $form.MinimumSize = $form.Size
     }
 #endregion
 
